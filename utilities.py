ce733dd16247cba498cf53ca43e487cc99d2b91a
VarunSundaram <varunksundaram@gmail.com>
2023-09-26 18:13:05 +0530

    added blob related code into python code 27

--- utilities.py
+++ utilities.py
@@ -1,7 +1,7 @@
 from azure.communication.email import EmailClient
 import os
 from constants import constants
-#from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
+from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
 import ssl
 import requests
 
@@ -41,5 +41,41 @@ def sendemail(lstGoodInstruments, bull=True):
         print (ex)
         return ex
 
+def uploadblob():
+    print (constants.TEMPHERE)
+    print(requests.certs.where())
+    #os.environ["HTTP_PROXY"] = "http://10.10.1.10:1180"
+    #os.environ["HTTPS_PROXY"] = "http://10.10.1.10:1180"
+
+    container_name = "lofty-cloud-blobs"
+    local_file_name = "access_credentials.json"
+    upload_file_path = os.path.join(constants.TEMPHERE, local_file_name)
+    STORAGEACCOUNTKEY = "wBuY2m+mdwDXiTF0SsYAhtShctvAwUp1+zgEnnwiTpls17+4NcapbrrnDMLzkE+7HzacqSyKhUFJ+AStf5K4vg=="
+    
+    account_url = "https://bulionbucket.blob.core.windows.net"
+    connection_string = "DefaultEndpointsProtocol=https;AccountName=bulionbucket;AccountKey=wBuY2m+mdwDXiTF0SsYAhtShctvAwUp1+zgEnnwiTpls17+4NcapbrrnDMLzkE+7HzacqSyKhUFJ+AStf5K4vg==;EndpointSuffix=core.windows.net"
+    ssl._create_default_https_context = ssl._create_unverified_context
+    # Create the BlobServiceClient object
+    blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+    container = blob_service_client.get_container_client(container=container_name)
+    
+    #blob_client = container.get_blob_client(container, local_file_name)
+    
+    # Create the BlobServiceClient object
+    #blob_client = BlobClient(account_url, container_name=container_name,
+    #        blob_name=local_file_name, credential=blob_service_client.credential)
+            #, proxies={ "https": "https://varunkumar.sundaram:asPire-12qwerty@10.10.1.10:1180" }
+    blob_client = blob_service_client.get_blob_client("https://bulionbucket.blob.core.windows.net/lofty-cloud-blobs"
+                                                    , blob=local_file_name)
+    
+    print (blob_client.blob_name)
+    # encoding param is necessary for readall() to return str, otherwise it returns bytes
+    downloader = blob_client.download_blob(max_concurrency=1, encoding='UTF-8')
+    blob_text = downloader.readall()
+    print("Blob contents: {blob_text}")
+    
+    # Upload the created file
+    with open(file=upload_file_path, mode="rb") as data:
+        blob_client.upload_blob(data)
 
 #uploadblob()
