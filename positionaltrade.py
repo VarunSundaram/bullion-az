ff415758c6e772e99d394f651957dfd1310f9871
VarunSundaram <varunksundaram@gmail.com>
2023-11-05 11:08:14 +0530

    full day event trigger 39

--- positionaltrade.py
+++ positionaltrade.py
@@ -233,6 +233,8 @@ def start_session():
         
         if (ut.download_blob(constants.INSTRUMENTS) == 0):
             logging.info ('going into ticker operation')
+            output = subprocess.check_output(["pwd"], text=True)
+            logging.info ("value return subprocess command.. " + str(output))
             output = subprocess.check_output(["python", "--version"], text=True)
             logging.info ("value return subprocess command.. " + str(output))
             output = subprocess.check_output(["ls", "-l"], text=True)
